7a5d4b43a4ec09eec8faca18a944154339d50c2e
joshua-8 <joshua_phelps@brown.edu>
2022-04-01 18:13:36 -0400

    prototype working

--- src/main.cpp
+++ src/main.cpp
@@ -16,15 +16,26 @@
 #include "loadAdjust/relaysDriver/relaysDriverBasic.h"
 #include "measurement/TachometerJEncoder.h"
 #include <Arduino.h>
+#include <QuickPID.h>
 
 // constants
-const byte numRelays = 8;
+/**
+ * @brief  how many relays are in your array?
+ * */
+const byte numRelays = 4;
 
 // declare instances of objects here:
-RelaysDriverBasic<numRelays> relayDriver = RelaysDriverBasic<numRelays>(LOW, { 16, 17, 18, 19, 21, 22, 23, 26 });
+RelaysDriverBasic<numRelays> relayDriver = RelaysDriverBasic<numRelays>(LOW, { 21, 22, 23, 26 }); // { 16, 17, 18, 19, 21, 22, 23, 26 });
 LoadAdjustRelayParallelBinary<numRelays> loadAdjust = LoadAdjustRelayParallelBinary<numRelays>(relayDriver);
 
-TachometerJEncoder tach = TachometerJEncoder(new JEncoderSingleAttachInterrupt(27, 1.0));
+JEncoderSingleAttachInterrupt tachEncoder = JEncoderSingleAttachInterrupt(27, 1.0, false, 100000, 10000); // pin, dist per count, reverse, slowest interval us, switchbounce us
+TachometerJEncoder tach = TachometerJEncoder(tachEncoder, 6);
+jENCODER_MAKE_ISR_MACRO(tachEncoder);
+
+float setpoint = 40;
+float input = 0;
+float output = 0;
+QuickPID pidControl = QuickPID(&input, &output, &setpoint);
 
 /**
  * @brief  called once on startup by Arduino
@@ -33,6 +44,12 @@ void setup()
 {
     Serial.begin(115200);
     loadAdjust.begin();
+    tachEncoder.setUpInterrupts(tachEncoder_jENCODER_ISR);
+
+    pidControl.SetOutputLimits(-.5, .5);
+    pidControl.SetSampleTimeUs(20000);
+    pidControl.SetTunings(.009, .009, 0);
+    pidControl.SetMode(QuickPID::Control::automatic); // turn on pid
 }
 
 /**
@@ -40,5 +57,18 @@ void setup()
  */
 void loop()
 {
-    loadAdjust.setLoad((millis() - 6000.0) / 30000.0);
+    tach.run();
+    input = tach.getMeasurement();
+    pidControl.Compute();
+    loadAdjust.setLoad(output + .5);
+
+    Serial.print(input / setpoint);
+    Serial.print(",");
+    Serial.print(output + .5);
+    Serial.print(",");
+    Serial.print(1.0);
+    Serial.print(",");
+
+    Serial.println();
+    delay(25);
 }
--- src/measurement/TachometerJEncoder.h
+++ src/measurement/TachometerJEncoder.h
@@ -3,25 +3,32 @@
 #include "JEncoder/JEncoder.h"
 #include "Measurement.h"
 #include <Arduino.h>
+#include <RunningAverage.h>
 /**
  * @brief  This class measures the speed of a generator using a tachometer.
  * @note you may use any of [these encoders](https://joshua-8.github.io/JMotor/class_j_encoder.html).
  **/
 class TachometerJEncoder : public Measurement {
+private:
+    float vel = 0;
+    RunningAverage smoother;
+
 public:
-    JEncoder* encoder;
-    TachometerJEncoder(JEncoder* _encoder)
-        : encoder(_encoder)
+    JEncoder& encoder;
+    TachometerJEncoder(JEncoder& _encoder, uint16_t numSavedForSmoothing = 0)
+        : smoother(numSavedForSmoothing + 1)
+        , encoder(_encoder)
     {
     }
 
     float getMeasurement()
     {
-        return encoder->getVel();
+        return smoother.getAverage();
     }
     void run()
     {
-        encoder->run();
+        encoder.run();
+        smoother.addValue(encoder.getVel());
     }
 };
 #endif
