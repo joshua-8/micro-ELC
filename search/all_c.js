6bc21b2e2dba9f1b340442f4348684013006deeb
joshua-8 <joshua-8@users.noreply.github.com>
2022-04-27 02:18:36 +0000

    deploy: af7413195374772ac5d5da7c21086c48f798d636

--- search/all_c.js
+++ search/all_c.js
@@ -4,7 +4,7 @@ var searchData=
   ['relaydriver_1',['relayDriver',['../main_8cpp.html#adf8b502aa1264686ce2f3ad265433aee',1,'main.cpp']]],
   ['relaysdriver_2',['RelaysDriver',['../class_relays_driver.html',1,'']]],
   ['relaysdriver_2eh_3',['RelaysDriver.h',['../_relays_driver_8h.html',1,'']]],
-  ['relaysdriverbasic_4',['RelaysDriverBasic',['../class_relays_driver_basic.html',1,'RelaysDriverBasic&lt; N &gt;'],['../class_relays_driver_basic.html#a8460c6eecf03bb3daa68dbcf22d0246c',1,'RelaysDriverBasic::RelaysDriverBasic()']]],
+  ['relaysdriverbasic_4',['RelaysDriverBasic',['../class_relays_driver_basic.html',1,'RelaysDriverBasic&lt; N &gt;'],['../class_relays_driver_basic.html#a8a239653fb8fd98b2d647b62739d3da8',1,'RelaysDriverBasic::RelaysDriverBasic()']]],
   ['relaysdriverbasic_2eh_5',['RelaysDriverBasic.h',['../_relays_driver_basic_8h.html',1,'']]],
   ['run_6',['run',['../class_measurement.html#aec68c0e7db76fd7bc70bdc389325e20a',1,'Measurement::run()'],['../class_tachometer_j_encoder.html#a499d846b8d4aaebf7637dcd585bb5575',1,'TachometerJEncoder::run()']]]
 ];
--- search/functions_4.js
+++ search/functions_4.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['relaysdriverbasic_0',['RelaysDriverBasic',['../class_relays_driver_basic.html#a8460c6eecf03bb3daa68dbcf22d0246c',1,'RelaysDriverBasic']]],
+  ['relaysdriverbasic_0',['RelaysDriverBasic',['../class_relays_driver_basic.html#a8a239653fb8fd98b2d647b62739d3da8',1,'RelaysDriverBasic']]],
   ['run_1',['run',['../class_measurement.html#aec68c0e7db76fd7bc70bdc389325e20a',1,'Measurement::run()'],['../class_tachometer_j_encoder.html#a499d846b8d4aaebf7637dcd585bb5575',1,'TachometerJEncoder::run()']]]
 ];
